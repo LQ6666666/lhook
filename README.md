08662b3554a18e212b8fac1eafc77d37ab2998f2
李琦 <171035726@qq.com>
2022-10-11 08:46:12 +0800

    feat: export useRequest

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Collection of vue hooks
+
+```shell
+# publish
+pnpm publish -r --force --access=public
+```
--- packages/lhook/src/index.ts
+++ packages/lhook/src/index.ts
@@ -1,4 +1,4 @@
 export * from "@lhook/boolean";
 export * from "@lhook/click-outside";
 export * from "@lhook/document-event";
-
+export * from '@lhook/request';
